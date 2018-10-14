674a654655a928f20679b5fbc5f09853909b129b
Taylor Weiss <taylor.weiss@comcast.net>
2018-10-14 12:15:02 -0700

    Add JSON response for menu item

--- Exercises/restaurants/flask_server.py
+++ Exercises/restaurants/flask_server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect, flash
+from flask import jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -32,6 +33,13 @@ def menu_items(restaurant_id):
     output = render_template( 'menu.html', restaurant = restaurant, menu = menu)
     session.close()
     return output
+
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_item_id>/JSON')
+def menu_item_json(restaurant_id, menu_item_id):
+    session = getDbSession()
+    menu_item = session.query(MenuItem).filter_by(id=menu_item_id).one()
+    return jsonify(MenuItem = menu_item.serialize)
 
 
 @app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_item_id>/edit', methods=['GET', 'POST'])
